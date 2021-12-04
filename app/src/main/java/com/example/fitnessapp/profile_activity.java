7cf77ff9c160825405c0d8f223f915cf94a9ffb6
ishashivalkar25 <ishashivalkar25@gmail.com>
2021-12-04 19:18:43 +0530

    Initial Commit 1

--- app/src/main/java/com/example/fitnessapp/profile_activity.java
+++ app/src/main/java/com/example/fitnessapp/profile_activity.java
@@ -41,7 +41,7 @@ public class profile_activity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_home, R.id.accountDetails, R.id.mentalWellness,R.id.reminders,R.id.reset2,R.id.logout)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_profile);
