e1075887e2a30b1ceeea434258c99db55db1f223
ishashivalkar25 <ishashivalkar25@gmail.com>
2021-12-04 14:12:11 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/fitnessapp/forgot_password_activity.java
@@ -0,0 +1,14 @@
+package com.example.fitnessapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class forgot_password_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
---
+++ app/src/main/java/com/example/fitnessapp/sign_up_activity.java
@@ -0,0 +1,15 @@
+package com.example.fitnessapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sign_up_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+
+}
