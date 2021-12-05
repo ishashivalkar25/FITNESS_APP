b558f43649e4fe79f6212379983c688d650459aa
ishashivalkar25 <ishashivalkar25@gmail.com>
2021-12-05 12:11:39 +0530

    Initial Commit 2

--- app/src/main/java/com/example/fitnessapp/profile_activity.java
+++ app/src/main/java/com/example/fitnessapp/profile_activity.java
@@ -1,12 +1,16 @@
 package com.example.fitnessapp;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
-
+import androidx.appcompat.widget.Toolbar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -15,12 +19,13 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.fitnessapp.databinding.ActivityProfileBinding;
+import com.google.android.material.tabs.TabLayout;
 
 public class profile_activity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityProfileBinding binding;
-
+    private  DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,25 +33,41 @@ public class profile_activity extends AppCompatActivity {
         binding = ActivityProfileBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.appBarProfile.toolbar);
-        binding.appBarProfile.fab.setOnClickListener(new View.OnClickListener() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
-        DrawerLayout drawer = binding.drawerLayout;
+
+        drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.accountDetails, R.id.mentalWellness,R.id.reminders,R.id.reset2,R.id.logout)
+                R.id.nav_home, R.id.accountDetails, R.id.mentalWellness,R.id.reminders,R.id.reset2,R.id.logout,R.id.nav_home,R.id.dietPlan,R.id.report)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_profile);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+        Log.i("msg","4");
+
+        BottomNavigationView bottom_nav_bar = findViewById(R.id.bottom_nav_bar);
+        NavigationUI.setupWithNavController(bottom_nav_bar,navController);
+
+//        bottom_nav_bar.setOnItemSelectedListener(new TabLayout.OnTabSelectedListener() {
+//            @Override
+//            public void onItemSelected(final int position) {
+//                // the user selected a new tab
+//            }
+//        });
     }
 
     @Override
@@ -62,4 +83,15 @@ public class profile_activity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
+    public void onBackPressed(){
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+
 }
